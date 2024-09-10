ea88f00af129711e801f8b1a095a4b018bc4b83d
jerome.fayot <jerome.fayot@gmail.com>
2024-09-10 11:42:47 +0200

    build: updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,18 @@ export default defineConfig({
       ionToken: "<YOUR_OWN_ION_TOKEN>"
     })
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            if (id.includes("react") || id.includes("react-dom"))
+              return "react";
+            if (id.includes("@cesium/engine")) return "cesium_engine";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
